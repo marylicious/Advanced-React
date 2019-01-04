045d24eef886966a39f95d03f14231ae9bbdb3c6
Maria V Ortiz <mariatua2@gmail>
2019-01-03 21:54:18 -0400

    updating

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -8,6 +8,31 @@ const mutations = {
         }, info);
 
         return item;
+    },
+
+    updateItem(parent,args,ctx,info){
+        //make a copy of args
+        const updates ={...args};
+        //delete id 
+        delete updates.id;
+        //run update method
+        return ctx.db.mutation.updateItem({
+            data:updates,
+            where:{
+                id: args.id
+            },
+        },info);
+    },
+
+    async deleteItem(parent,args,ctx,info) {
+        
+        const where = {id: args.id};
+        //find the item
+        const item= await ctx.db.query.item({where}, `{id title}`);
+
+        //check if they own that item or have permissions
+        //delete it
+        return ctx.db.mutation.deleteItem({where}, info);
     }
     
 };
---
+++ sick-fits/frontend/components/Pagination.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PaginationStyles from './styles/PaginationStyles';
+import gql from 'graphql-tag';
+import {Query} from 'react-apollo';
+import {perPage} from '../config';
+import Head from 'next/head';
+import Link from 'next/link';
+
+const PAGINATION_QUERY=gql`
+    query PAGINATION_QUERY{
+        itemsConnection {
+                aggregate {
+                count
+            }
+        }
+    }
+`;
+
+
+const Pagination = props =>(
+    
+        <Query query={PAGINATION_QUERY} >
+        
+        {({data,loading,error})=>{
+            if(loading) return <p>Loading...</p>
+            const count=data.itemsConnection.aggregate.count;
+            const pages= Math.ceil((count / perPage));
+            const page=props.page;
+            return(
+                <PaginationStyles>
+                    <Head>
+                        <title>Sick Fits! - Page {page} of {pages} </title>
+                    </Head>
+                    <p>Page {page} of {pages} </p>
+                </PaginationStyles>
+            )
+        }
+        }
+        </Query>
+);
+
+export default Pagination;
